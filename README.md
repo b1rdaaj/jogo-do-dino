09dade600509c379cc2fd48a2aa0bc52b0eb8b2c
Anna Carolina de Araujo Pereira <u676676@brb.com.br>
2025-08-21 16:39:21 -0300

    atualizando projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jogo-do-dino
 joguinho do Dino de quando estamos sem internet
+futuras atualizações: colocar um contador de pulos (acertos) e zerar quando perder.
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0; //inicia no 0
 
 function handleKeyUp(event) { 
     if (event.keyCode === 32) {
@@ -11,12 +12,11 @@ function handleKeyUp(event) {
 } //essa função diz para "pular" quando a tecla 32 (teclado) for pressionado
 
 function jump() {
-    let position = 0; //inicia no 0
     isJumping = true;
 
 //subindo
     let upInterval = setInterval(() => {
-        if (position >= 150){
+        if (position >= 300){
             clearInterval(upInterval);
 //descendo
         let downInterval = setInterval(() => {
@@ -34,26 +34,30 @@ function jump() {
         }
 }, 20);//tempo em milisegundos que ele sobe
 }
-
+//aqui vamos criar o cactus para o dino pular
 function createCactus () {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random () * 6000;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
-    let randomTime = Math.random () * 6000;
 
     let leftInterval = setInterval(() => {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
-            background.removeChild(cactus);
+            background.removeChild(cactus);//ele some dps de usa-lo.
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //game over!
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over"> Fim de Jogo! <br> :((((((</h1>';
         } else {
             cactusPosition -= 10; //velocidade que vai para a esquerda 
             cactus.style.left = cactusPosition + 'px';
         }
     }, 20)
-    
+
     setTimeout(createCactus, randomTime)
 
 }
